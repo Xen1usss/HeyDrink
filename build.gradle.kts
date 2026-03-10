dd4978c87368845a428c98b5082707ac44b45637
Ksenia <130775320+Xen1usss@users.noreply.github.com>
2026-03-10 20:45:25 +0300

    Changes by user

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.hilt) apply false
-    id("com.google.devtools.ksp") version "2.3.4" apply false
+    alias(libs.plugins.android.ksp) apply false
 }
