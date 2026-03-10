a0fa48bed872e23177ba3530ca2b832def75a99b
Ksenia <130775320+Xen1usss@users.noreply.github.com>
2026-03-10 20:26:23 +0300

    add Hilt libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -86,4 +88,7 @@ dependencies {
     implementation("com.google.accompanist:accompanist-navigation-animation:0.36.0")
 
     implementation("androidx.compose.material:material-icons-extended")
+
+    implementation("com.google.dagger:hilt-android:2.57.1")
+    ksp("com.google.dagger:hilt-android-compiler:2.57.1")
 }
